50f9cf17f937a259fef8ea33a58401f95710d27c
Ryan Wolfslayer <rdw717756@gmail.com>
2024-06-29 16:39:16 -0400

    Fixing bug where required directories were not being created

--- run.py
+++ run.py
@@ -1,6 +1,19 @@
-# run.py
+import os
+
+def create_directories():
+    # Get the directory of the current script
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    directories = ["processed", "chunks", "uploads"]
+    for directory in directories:
+        path = os.path.join(base_dir, directory)
+        if not os.path.exists(path):
+            os.makedirs(path)
 
 from textract_ssml_processor import create_app
+
+# Create necessary directories
+create_directories()
 
 app = create_app()
 
